b3fd8390f1570e1530bac0353cc0f1011653e04c
cgrobbin <cgrobbin@gmail.com>
2021-04-05 16:56:08 -0500

    Auth setup

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import uuid
 import boto3
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
+from django.contrib.auth import login
 
 S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com/'
 BUCKET = 'recipemaster-capstone-0119'
@@ -15,8 +18,23 @@ def about(request):
 
 # # Profile/Recipe Detail
 # # To add to correct folder, add folder name to url s3/bucket/folder/key
-# def profile(request):
-#     return
+def profile(request):
+    return render(request, 'profile.html')
+
+# Signup
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+        else:
+            error_message = 'Invalid Signup - Try Again'
+    form = SignupForm()
+    context = { 'form': form, 'error_message': error_message }
+    return render(request, 'registration/signup.html', context)
 
 # # Recipe Index
 # def recipe_index(request):
