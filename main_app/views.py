e64573ffd0fcd53442db84e98b0df2efc7835e2b
cgrobbin <cgrobbin@gmail.com>
2021-04-04 20:31:42 -0500

    Init setup for aws, reminder for later

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+import uuid
+import boto3
 
-# Create your views here.
+S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com/'
+BUCKET = 'recipemaster-capstone-0119'
+
+# Home
 def home(request):
     return render(request, 'home.html')
 
+# About
 def about(request):
     return render(request, 'about.html')
+
+# Profile/Recipe Detail
+# To add to correct folder, add folder name to url, ****TALK TO TA OR INSTRUCTOR ABOUT CONFIGURATION****
