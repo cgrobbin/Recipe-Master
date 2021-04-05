bb3afd45916118f1c244cc63a8051746a1d6abfa
cgrobbin <cgrobbin@gmail.com>
2021-04-05 14:31:51 -0500

    Database setup for recipes, adjustments for photos and arrays

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
-    url = models.CharField(max_length=250)
+    url = models.CharField(
+        max_length=250,
+        default="https://i.imgur.com/CMqTyEZs.jpg"
+        )
 
     def __str__(self):
         return f"{self.user}"
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    prep_time = models.CharField(max_length=25)
+    cook_time = models.CharField(max_length=25)
+    servings = models.IntegerField()
+    ingredients = ArrayField(models.CharField(max_length=50))
+    instructions = ArrayField(models.TextField(max_length=250))
+    url = models.CharField(
+        max_length=250,
+        default="https://i.imgur.com/RtCoQcll.jpg",
+        )
+    tags = ArrayField(models.CharField(max_length=50))
+
+    def __str__(self):
+        return f"{self.title}"
     
+
