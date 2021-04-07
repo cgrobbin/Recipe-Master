e7b5a30d8ee19c3da8f6df2e2714845f76bfd3c6
cgrobbin <cgrobbin@gmail.com>
2021-04-07 17:40:25 -0500

    Comment form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Recipe
+from .models import Recipe, Comment
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField()
@@ -19,3 +19,8 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
